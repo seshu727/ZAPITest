fb862c5002a05262a0ff52eb014d1dcc9ac1e05a
SESHU <SESHU@DESKTOP-RJVCUVF>
2020-09-25 17:05:28 +0530

    ok

---
+++ Test3/src/test/java/Git_Actions/Launch.java
@@ -0,0 +1,24 @@
+package Git_Actions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Launch {
+	static WebDriver driver;
+	@BeforeTest
+	public static void setup() throws Throwable {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\SESHU\\Talentas\\Talent_Workspace\\webApp_Purchase_Insight_Tool\\Drivers\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.get("https://google.com");
+		driver.manage().window().maximize();
+	}
+	@Test
+	public void sampleTest() {
+		System.out.println("done");
+		Assert.assertEquals(true, "done");
+	}
+}
