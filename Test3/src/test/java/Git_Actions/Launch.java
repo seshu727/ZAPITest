2c72b035be900d38091377cb886aac42a3f13166
SESHU <pseshu350@gmail.com>
2020-09-25 18:48:26 +0530

    update

--- Test3/src/test/java/Git_Actions/Launch.java
+++ Test3/src/test/java/Git_Actions/Launch.java
@@ -1,14 +1,15 @@
 package Git_Actions;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+
 
 public class Launch {
 	static WebDriver driver;
-	@BeforeTest
+	@Before
 	public static void setup() throws Throwable {
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\SESHU\\Talentas\\Talent_Workspace\\webApp_Purchase_Insight_Tool\\Drivers\\chromedriver.exe");
